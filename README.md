c5208089bb1cc1e09807a68998380aae23fe58b0
Your Name <erikahendsel@hotmail.com>
2023-09-30 14:32:29 +0200

    fetching data of pokemon created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-rnfe
+rnfe - command to generate default react native template
+j - in cmd, if you press 'j' while running the app, it will open the browser console log.
+r - in cmd, if you press 'r' while running the app, it will reload. So you can basically see what happens on initial load of the app.
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,19 @@
+import { Pokemon, getPokemon } from "@/api/pokeapi";
 import { Link } from "expo-router";
+import { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 
 const Page = () => {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const result = await getPokemon();
+      console.log("result:", result);
+    };
+    load();
+  }, []);
+
   return (
     <View>
       <Link href={"/(pokemon)/test"}>
