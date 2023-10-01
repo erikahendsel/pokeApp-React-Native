b76973cd98eea31470b91750a3afee635186db6f
Your Name <erikahendsel@hotmail.com>
2023-10-01 15:17:54 +0200

    home page has styling and poke list

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
 import { Pokemon, getPokemon } from "@/api/pokeapi";
 import { Link } from "expo-router";
 import { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
 
 const Page = () => {
   const [pokemon, setPokemon] = useState<Pokemon[]>([]);
@@ -9,18 +11,46 @@ const Page = () => {
   useEffect(() => {
     const load = async () => {
       const result = await getPokemon();
-      console.log("result:", result);
+      setPokemon(result);
     };
     load();
   }, []);
 
   return (
-    <View>
-      <Link href={"/(pokemon)/test"}>
-        <Text>Details</Text>
-      </Link>
-    </View>
+    <ScrollView>
+      {pokemon.map((p) => (
+        // asChild allows us to use styling of the children
+        <Link href={`/(pokemon)/${p.id}`} key={p.id} asChild>
+          <TouchableOpacity>
+            <View style={styles.item}>
+              <Image source={{ uri: p.image }} style={styles.preview} />
+              <Text style={styles.itemText}>{p.name}</Text>
+              <Ionicons name="chevron-forward" size={24} />
+            </View>
+          </TouchableOpacity>
+        </Link>
+      ))}
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    height: 100,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemText: {
+    fontSize: 18,
+    textTransform: "capitalize",
+    flex: 1,
+  },
+  preview: {
+    width: 100,
+    height: 100,
+  },
+});
 
 export default Page;
